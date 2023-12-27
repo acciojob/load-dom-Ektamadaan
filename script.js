f16c51383fda0f748f3fb0bcbcd3377aca5dd4bc
Ektamadaan <ektamadaan45@gmail.com>
2023-12-27 20:59:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 
 
-  document.addEventListener("DOMContentLoaded", function () { 
-    var paragraph = document.createElement("p");
-	  var textNode = document.createTextNode("DOM load success");
-    paragraph.appendChild(textNode);
-    document.body.appendChild(paragraph);
-  });
+ 
